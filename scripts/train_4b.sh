d562e5ce20fa806508bdbfa838d4bfa7ea5b6cf2
Zhaopeng Xuan <xuanzhaopeng@gmail.com>
2026-01-24 17:32:35 +0100

    Support 4b training

---
+++ scripts/train_4b.sh
@@ -0,0 +1,19 @@
+
+PROJECT_DIR="$(pwd)"
+CONFIG_PATH="$PROJECT_DIR/configs"
+
+
+echo "Start training curriculum"
+echo "Loading configs $CONFIG_PATH"
+
+CUDA_VISIBLE_DEVICES=0 python -m curriculum.start_test \
+    --config-path="$CONFIG_PATH" \
+    --config-name='curriculum_config' \
+    hydra.job.chdir=False \
+    actor_rollout_ref.model.path=Qwen/Qwen3-4B-Base \
+    trainer.total_training_steps=1 \ 
+    trainer.total_epochs=1 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.3
+    
+
+echo "curriculum agent training finished"
