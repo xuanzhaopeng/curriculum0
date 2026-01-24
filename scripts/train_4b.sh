b5c306d429930501d540c6953783b87f711f8402
Zhaopeng Xuan <xuanzhaopeng@gmail.com>
2026-01-24 18:51:52 +0100

    try to enable f2

--- scripts/train_4b.sh
+++ scripts/train_4b.sh
@@ -2,18 +2,13 @@
 PROJECT_DIR="$(pwd)"
 CONFIG_PATH="$PROJECT_DIR/configs"
 
-# === 2. 核心修复 (这就是刚才测试通过的原因) ===
-# 强制让 VeRL 使用和刚才测试脚本一样的通信配置
+# === 支持双卡通信 ===
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 export NCCL_SOCKET_IFNAME=eth0
 export GLOO_SOCKET_IFNAME=eth0
-export NCCL_DEBUG=INFO  # 保持日志开启，万一有问题能看到
+export NCCL_DEBUG=INFO 
 
-# === 3. 强制兼容模式 (防止 FlashAttn 导致的 Kernel 死锁) ===
-# 虽然机器没坏，但为了稳过，我们先禁用 FlashAttn，跑通再说
-export USE_FLASH_ATTENTION=0
-export VLLM_ATTENTION_BACKEND=XFORMERS
 
 echo "Start training curriculum"
 echo "Loading configs $CONFIG_PATH"
@@ -32,8 +27,6 @@ CUDA_VISIBLE_DEVICES=0,1 python -m curriculum.start_test \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     actor_rollout_ref.rollout.max_num_seqs=1024 \
     actor_rollout_ref.rollout.max_model_len=8192 \
-    actor_rollout_ref.model.override_config.attn_implementation=eager \
     actor_rollout_ref.rollout.engine_kwargs.vllm="{max_num_seqs: 1024, max_model_len: 8192}" \
-    actor_rollout_ref.rollout.enforce_eager=True
 
 echo "curriculum agent training finished"
